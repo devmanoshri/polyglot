159ac0c1886da2b4912480b19bc2ba3d3299d9e7
devmanoshri <manoshri.no@gmail.com>
2022-12-16 22:41:58 +0100

    update 8

--- src/form-elements/Checkbox.js
+++ src/form-elements/Checkbox.js
@@ -4,13 +4,14 @@ function Checkbox({ value, name, id, labelText, ifChecked, onChangeHandel }) {
   return (
     <div className="form-check">
       <input
-        onChange={onChangeHandel}
         className="form-check-input"
         type="checkbox"
         value={value}
         id={id}
         name={name}
-
+        onChange={(e) => {
+          onChangeHandel(e);
+        }}
         //{ifChecked === 1 ? "checked" : null}
         // {checked && "checked"}
       />
