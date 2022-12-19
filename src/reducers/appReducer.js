2fc875c9e3f9a0989a00a404b95dbff1ce2a2de4
devmanoshri <manoshri.no@gmail.com>
2022-12-19 10:59:00 +0100

    update

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -3,13 +3,6 @@ const initialState = [];
 export const appReducer = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_APPLICATION":
-      // console.log([
-      //   ...state,
-      //   {
-      //     appName: action.payload.appName,
-      //     languageList: action.payload.languageList,
-      //   },
-      // ]);
       return [
         ...state,
         {
